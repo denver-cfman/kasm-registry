fd7970c45abd9d4cf0be59cc3e9a8838039ef453
Mr Z <denver.cfman@gmail.com>
2024-01-20 05:16:46 -0700

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Giezen Consulting',
+    description: 'Giezen Consulting Kasm supported workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://denver-cfman.github.io/kasm-registry/',
+    contactUrl: 'https://support.giezenconsulting.com',
   },
   reactStrictMode: true,
   swcMinify: true,
